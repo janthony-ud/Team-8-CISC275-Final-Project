d18983c033faa4250901e693d9daabaf0828d909
Brad Daugherty <braded@udel.edu>
2023-04-13 15:06:06 -0400

    Created movie interface

---
+++ src/interfaces/movie.ts
@@ -0,0 +1,15 @@
+/** Object representing a movie from the past year (2023) with relevant information */
+export interface Movie {
+    /** The title of the movie*/
+    title: string;
+    /** A short description of the movie plot */
+    description: string;
+    /** The age rating for who should be allowed to watch the movie */
+    maturity_rating: string;
+    /** Short list of the main cast members (10 maximum) */
+    cast: string[];
+    /** List of genres that the movie falls under */
+    genre: string[];
+
+    /** Will add more attributes as needed */
+}
