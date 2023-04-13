3d1296ce39b07bd85b001e317daf2676b357df5b
mmgamble <mmgamble@udel.edu>
2023-04-13 09:28:06 -0400

    Setup npm run start with files - corrected others

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+//import { Container } from "react-bootstrap";
+//import { Row } from "react-bootstrap";
+//import { Col } from "react-bootstrap";
+//import { Button } from "react-bootstrap";
+
+import "./App.css";
+
+function App(): JSX.Element {
+    return (
+        <header className="App-header">
+            <p>Movie Rating App</p>
+        </header>
+    );
+}
+
+export default App;
