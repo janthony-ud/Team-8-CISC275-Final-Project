c5a1655a39bfa69adf6fd868d215e67c49c18c3a
Ryan Sanchez <ryansan@udel.edu>
2023-04-20 14:54:52 -0400

    adding monitor

--- src/components/CentralList.tsx
+++ src/components/CentralList.tsx
@@ -1,18 +1,39 @@
 import React from "react";
 import movieList from "../data/movieList.json";
 import { Movie } from "../interfaces/movie";
+import { useDrag, DragSourceMonitor } from "react-dnd";
+import { useState } from "react";
+
+interface MovieDragItem {
+    type: string;
+    payload: Movie | null;
+}
 
 export function CentralList(): JSX.Element {
-    const movies: Movie[] = movieList.map((movie) => {
-        return {
-            image: movie.image,
-            title: movie.name,
-            description: movie.desc,
-            maturity_rating: movie.age,
-            cast: movie.cast,
-            genre: movie.genre
-        };
+    const [movies, setMovies] = useState<Movie[]>(
+        movieList.map((movie) => {
+            return {
+                image: movie.image,
+                title: movie.name,
+                description: movie.desc,
+                maturity_rating: movie.age,
+                cast: movie.cast,
+                genre: movie.genre
+            };
+        })
+    );
+
+    const [draggedMovie, setDraggedMovie] = useState<Movie | null>(null);
+    const [, drag] = useDrag<MovieDragItem, void, { isDragging: boolean }>({
+        item: { type: "movie", payload: null },
+        collect: (monitor: DragSourceMonitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
     });
+
+    // const handleMouseDown = (movie: Movie) => {
+    //     setDraggedMovie(movie);
+    // };
 
     return (
         <div>
@@ -20,7 +41,12 @@ export function CentralList(): JSX.Element {
 
             <div className="col">
                 {movies.map((movie) => (
-                    <div key={movie.title}>
+                    <div
+                        key={movie.title}
+                        ref={drag}
+                        // onMouseDown={() => handleMouseDown(movie)}
+                        // style={{ opacity: isDragging ? 0.5 : 1 }}
+                    >
                         <div>
                             <img src={movie.image} alt={movie.title} />
                             <h3>{movie.title}</h3>
