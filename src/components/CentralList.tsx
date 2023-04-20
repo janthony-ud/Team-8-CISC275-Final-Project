c620c582a306ce120aa2fd8c849fd5150802bc4d
mmgamble <mmgamble@udel.edu>
2023-04-20 01:53:37 -0400

    Resolving errors

--- src/components/CentralList.tsx
+++ src/components/CentralList.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import movieList from "../data/movieList.json";
 import { Movie } from "../interfaces/movie";
-import { Button } from "react-bootstrap";
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
-import { Container } from "react-bootstrap";
 
 export function CentralList(): JSX.Element {
     const movies: Movie[] = movieList.map((movie) => {
@@ -31,9 +27,9 @@ export function CentralList(): JSX.Element {
                         </div>
                         <div>
                             <p>{movie.description}</p>
-                            <p>Genre: {movie.genre}</p>
+                            <p>Genre: {movie.genre.join(", ")}</p>
                             <p>Age Rating: {movie.maturity_rating}</p>
-                            <p>Cast: {movie.cast}</p>
+                            <p>Cast: {movie.cast.join(", ")}</p>
                         </div>
                     </div>
                 ))}
