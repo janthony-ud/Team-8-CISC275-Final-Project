6a45742736622ede0e5870da7e498d676dae1774
mmgamble <mmgamble@udel.edu>
2023-05-05 09:55:06 -0400

    Super List

--- src/components/ChooseRole.tsx
+++ src/components/ChooseRole.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useState } from "react";
 import initialUsers from "../data/initialUsers.json";
 import { User } from "../interfaces/user";
-import NewUserButton from "./User";
+import NewUserButton from "./NewUserButton";
 import YourList from "./UserList";
 import { CentralList } from "./CentralList";
 import "./ChooseRole.css";
 import { AdminList } from "./AdminList";
 import { Avatar, AvatarBadge, Stack, Box } from "@chakra-ui/core";
 import { Tooltip } from "@chakra-ui/core";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/core";
 
 const ChooseUser: React.FC = () => {
     const [users, setUsers] = useState<User[]>(
@@ -43,11 +44,59 @@ const ChooseUser: React.FC = () => {
         } else if (user.role == "super") {
             return (
                 <div>
-                    <NewUserButton
-                        onSubmit={function (newUser: User): void {
-                            setUsers((prevUsers) => [...prevUsers, newUser]);
-                        }}
-                    ></NewUserButton>
+                    <Tabs>
+                        <TabList>
+                            <Tab>Create/Delete Users</Tab>
+                            <Tab>View/Edit Admin List</Tab>
+                            <Tab>Add/Delete Movies</Tab>
+                        </TabList>
+
+                        <TabPanels>
+                            <TabPanel>
+                                <NewUserButton
+                                    onSubmit={function (newUser: User): void {
+                                        setUsers((prevUsers) => [
+                                            ...prevUsers,
+                                            newUser
+                                        ]);
+                                    }}
+                                ></NewUserButton>
+                                {users.map((user) => (
+                                    <div key={user.name}>
+                                        <div className="induseravatar">
+                                            <Tooltip
+                                                label={handleToolTip(user)}
+                                                placement="top"
+                                                aria-label="Hello"
+                                                shouldWrapChildren={true}
+                                            >
+                                                <Avatar
+                                                    onClick={() =>
+                                                        handleSetUser(user)
+                                                    }
+                                                    as="a"
+                                                    name={user.name}
+                                                    src="https://bit.ly/broken-link"
+                                                    // backgroundColor={setAvatarColor(user)}
+                                                >
+                                                    <AvatarBadge
+                                                        bg={setBadgeColor(user)}
+                                                        size="0.75em"
+                                                    />
+                                                </Avatar>
+                                            </Tooltip>
+                                        </div>
+                                    </div>
+                                ))}
+                            </TabPanel>
+                            <TabPanel>
+                                <p>View/Edit Admin List</p>
+                            </TabPanel>
+                            <TabPanel>
+                                <p>Add a New Movie</p>
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
                 </div>
             );
         } else {
