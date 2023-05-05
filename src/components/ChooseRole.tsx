03323112684a9d4934710196eb93c4bce9fe3ef3
mmgamble <mmgamble@udel.edu>
2023-05-05 19:41:09 -0400

    Working on Super

--- src/components/ChooseRole.tsx
+++ src/components/ChooseRole.tsx
@@ -9,6 +9,9 @@ import "./ChooseRole.css";
 import { AdminList } from "./AdminList";
 import { Avatar, AvatarBadge, Stack, Box } from "@chakra-ui/core";
 import { Tooltip } from "@chakra-ui/core";
+import { Button } from "@chakra-ui/core";
+//import { Icon } from "@chakra-ui/core";
+import { useEffect } from "react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/core";
 
 const ChooseUser: React.FC = () => {
@@ -23,8 +26,53 @@ const ChooseUser: React.FC = () => {
     );
     const [currentUser, setCurrentUser] = useState<User>(users[0]);
 
+    useEffect(() => {
+        const storedUsers = localStorage.getItem("users");
+        if (storedUsers) {
+            setUsers(JSON.parse(storedUsers));
+        } else {
+            setUsers(
+                initialUsers.map((user) => ({
+                    name: user.name,
+                    userMovieList: user.userMovieList,
+                    role: user.role
+                }))
+            );
+        }
+    }, [users]);
+
+    useEffect(() => {
+        localStorage.setItem("users", JSON.stringify(users));
+    }, []);
+
     function handleSetUser(user: User) {
         setCurrentUser(user);
+    }
+
+    function removeUser(name: string): void {
+        setUsers([...users].filter((user) => user.name !== name));
+        localStorage.setItem(
+            "users",
+            JSON.stringify([...users].filter((user) => user.name !== name))
+        );
+    }
+
+    function handleRemoveUser(user: User) {
+        if (user.role == "super") {
+            return "";
+        } else {
+            return (
+                <div className="remove-movie">
+                    <Button
+                        variantColor="red"
+                        size="xs"
+                        onClick={() => removeUser(user.name)}
+                    >
+                        Delete User
+                    </Button>
+                </div>
+            );
+        }
     }
 
     function handleUserType(user: User) {
@@ -53,38 +101,32 @@ const ChooseUser: React.FC = () => {
 
                         <TabPanels>
                             <TabPanel>
+                                <h1>Create/Delete Users</h1>
                                 <NewUserButton
                                     onSubmit={function (newUser: User): void {
                                         setUsers((prevUsers) => [
                                             ...prevUsers,
                                             newUser
                                         ]);
+                                        localStorage.setItem(
+                                            "users",
+                                            JSON.stringify([...users, newUser])
+                                        );
                                     }}
                                 ></NewUserButton>
                                 {users.map((user) => (
                                     <div key={user.name}>
                                         <div className="induseravatar">
-                                            <Tooltip
-                                                label={handleToolTip(user)}
-                                                placement="top"
-                                                aria-label="Hello"
-                                                shouldWrapChildren={true}
+                                            <h3>
+                                                {user.name}, {user.role}{" "}
+                                            </h3>
+                                            <Button
+                                                variantColor="green"
+                                                size="sm"
                                             >
-                                                <Avatar
-                                                    onClick={() =>
-                                                        handleSetUser(user)
-                                                    }
-                                                    as="a"
-                                                    name={user.name}
-                                                    src="https://bit.ly/broken-link"
-                                                    // backgroundColor={setAvatarColor(user)}
-                                                >
-                                                    <AvatarBadge
-                                                        bg={setBadgeColor(user)}
-                                                        size="0.75em"
-                                                    />
-                                                </Avatar>
-                                            </Tooltip>
+                                                View List
+                                            </Button>{" "}
+                                            {handleRemoveUser(user)}
                                         </div>
                                     </div>
                                 ))}
