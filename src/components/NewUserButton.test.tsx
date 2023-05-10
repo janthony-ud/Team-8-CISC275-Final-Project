69a9cceb0c8bdb9852118d9e6be41d666be38907
Justin <janthony@udel.edu>
2023-05-09 22:48:58 -0400

    figuring out new window opening test

--- src/components/NewUserButton.test.tsx
+++ src/components/NewUserButton.test.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { User } from "../interfaces/user";
-import ReactDOM from "react-dom";
-import { userMovie } from "../interfaces/userMovie";
-import { Button, ThemeProvider } from "@chakra-ui/core";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+//import ReactDOM from "react-dom";
+//import { userMovie } from "../interfaces/userMovie";
+import { ThemeProvider } from "@chakra-ui/core";
+import { render, screen } from "@testing-library/react";
 import NewUserButton from "./NewUserButton";
 
 const mockusers = ["alice", "bob", "charlie"];
@@ -21,10 +21,21 @@ describe("New User Button component tests", () => {
         );
     });
 
-    test("new user button appears on screen", () => {
+    test("new-user-button appears on screen", () => {
         const nub = screen.getByRole("button", {
             name: /Create A New User:/i
         });
         expect(nub).toBeInTheDocument();
     });
+    /*
+    test("clicking button takes you to new web page", () => {
+        const nub = screen.getByRole("button", {
+            name: /Create A New User:/i
+        });
+        fireEvent.click(nub);
+        //const newWindow = screen.getByTestId("Create a New User");
+        //expect().toBeInTheDocument();
+        //Need to figure out how to test a new window is opening in jest
+    });
+    */
 });
