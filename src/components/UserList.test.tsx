59f8855b6778c23f6bb0a9b660c9b67eec46dc7f
Ryan Sanchez <ryansan@udel.edu>
2023-05-19 00:45:08 -0400

    added userlist tests

--- src/components/UserList.test.tsx
+++ src/components/UserList.test.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import UserList from "./CentralList";
 import { ThemeProvider } from "@chakra-ui/core/dist";
-import { Movie } from "../interfaces/movie";
+import { userMovie } from "../interfaces/userMovie";
+import YourList from "./UserList";
 
 const testUser = {
     name: "test user",
@@ -20,7 +21,7 @@ const testUser = {
 //     user_rating: 0
 // };
 
-const mockList: Movie[] = [
+const mockList: userMovie[] = [
     {
         image: "",
         title: "A Movie",
@@ -28,7 +29,8 @@ const mockList: Movie[] = [
         maturity_rating: "PG",
         cast: ["Bob", "Mary", "Jim"],
         genre: ["Adventure"],
-        user_rating: 0
+        user_rating: 0,
+        id: 0
     },
     {
         image: "",
@@ -37,7 +39,8 @@ const mockList: Movie[] = [
         maturity_rating: "G",
         cast: ["Bob", "Mary", "Jim"],
         genre: ["Adventure"],
-        user_rating: 0
+        user_rating: 0,
+        id: 1
     },
     {
         image: "",
@@ -46,7 +49,8 @@ const mockList: Movie[] = [
         maturity_rating: "R",
         cast: ["Bob", "Mary", "Jim"],
         genre: ["Adventure"],
-        user_rating: 0
+        user_rating: 0,
+        id: 2
     }
 ];
 
@@ -83,5 +87,35 @@ describe("User List Tests", () => {
         fireEvent.click(sortBtn);
         const sortTitle = screen.getByText("Title");
         fireEvent.click(sortTitle);
+    });
+    test("renders Sort button for user", () => {
+        render(
+            <ThemeProvider>
+                <UserList user={testUser} handling={"userList"} />
+            </ThemeProvider>
+        );
+        const sort = screen.queryAllByText("Sort");
+        expect(sort).not.toBeNull();
+    });
+    test("dropdown options visible aft button click", () => {
+        render(
+            <ThemeProvider>
+                <UserList user={testUser} handling={""} />
+            </ThemeProvider>
+        );
+        const sortBtn = screen.getByText("Sort");
+        fireEvent.click(sortBtn);
+        const sortTitle = screen.getByText("Title");
+        const sortMaturity = screen.getByText("Maturity");
+        expect(sortTitle).toBeInTheDocument();
+        expect(sortMaturity).toBeInTheDocument();
+    });
+    test("renders no monvies when list is empty", () => {
+        render(
+            <ThemeProvider>
+                <UserList user={testUser} handling={""} />
+            </ThemeProvider>
+        );
+        expect(screen.queryByText("Remove")).not.toBeInTheDocument();
     });
 });
